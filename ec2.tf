5662aa0e5556101ea780ea95528a43f4f1f872cd
subhash <vpsubhash2002@gmail.com>
2024-03-16 00:37:20 +0530

    first commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIAQBZRCLADARPJOKM7"
-    secret_key = "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"
+    
 }
 
 resource "aws_instance" "ec2" {
